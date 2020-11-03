19f51ceff9c9bc04b19d0bbade788a3740ecb235
ruby <rubypoohtwm@gmail.com>
2020-11-03 23:06:21 +0800

    註冊modal初步完成2

--- final/src/pages/Login.js
+++ final/src/pages/Login.js
@@ -53,12 +53,12 @@ function MemberLoginModal(props) {
       .catch((error) => {})
   }
   //按下modal裡的「註冊」觸發的function
-  function RegisterSubmit(){
-    
-  }
+  function RegisterSubmit() {}
 
   //登入或註冊的狀態
-  const [loginOrRegister, setLoginOrRegister] =useState(true)
+  const [loginOrRegister, setLoginOrRegister] = useState(true)
+
+  //登入
   const loginOrRegisterTrue = (
     <>
       <Modal.Header closeButton>
@@ -108,12 +108,20 @@ function MemberLoginModal(props) {
         </form>
       </Modal.Body>
       <Modal.Footer>
-        <p className="register_info" onClick={()=>{setLoginOrRegister(false)}}>還沒有帳號嗎？點我註冊</p>
+        <p
+          className="register_info"
+          onClick={() => {
+            setLoginOrRegister(false)
+          }}
+        >
+          還沒有帳號嗎？點我註冊
+        </p>
         <Button onClick={loginInfoSubmit}>{isAuth ? '登出' : '登入'}</Button>
       </Modal.Footer>
     </>
   )
 
+  //註冊
   const loginOrRegisterFalse = (
     <>
       <Modal.Header closeButton>
@@ -122,48 +130,108 @@ function MemberLoginModal(props) {
       <Modal.Body>
         <form>
           <div className="form-group">
-            <label htmlFor="loginEmail">電子郵件</label>
+            <label htmlFor="registerName">顯示暱稱</label>
             <input
-              type="email"
+              type="text"
               className="form-control"
-              id="loginEmail"
-              aria-describedby="emailHelp"
-              placeholder="請輸入電子郵件"
-              onChange={(e) => {
-                const newMeberLoginEmail = e.target.value
-                setMemberLoginEmail(newMeberLoginEmail)
-              }}
+              id="registerName"
+              placeholder="請輸入暱稱"
+              onChange={() => {}}
             />
           </div>
           <div className="form-group">
-            <label htmlFor="loginPwd">密碼</label>
+            <label htmlFor="registerEmail">電子郵件</label>
+            <input
+              type="email"
+              className="form-control"
+              id="registerEmail"
+              aria-describedby="emailHelp"
+              placeholder="請輸入電子郵件"
+              onChange={() => {}}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="registerPwd">密碼</label>
             <input
               type="password"
               className="form-control"
-              id="loginPwd"
+              id="registerPwd"
               placeholder="請輸入密碼"
-              onChange={(e) => {
-                const newMeberLoginPwd = e.target.value
-                setMemberLoginPwd(newMeberLoginPwd)
-              }}
+              onChange={()=>{}}
             />
           </div>
           <div className="form-group form-check">
             <input
               type="checkbox"
               className="form-check-input"
-              id="loginpwdCheck"
-              onClick={pwdCheck}
+              id="registerpwdCheck"
+              onClick={()=>{}}
             />
-            <label className="form-check-label" htmlFor="loginpwdCheck">
+            <label className="form-check-label" htmlFor="registerpwdCheck">
               顯示密碼
             </label>
-            <small id="loginsmallinfo" className="form-text text-muted"></small>
+            <small id="registersmallinfo" className="form-text text-muted"></small>
           </div>
+          <div className="form-group">
+                <label htmlFor="registerGender">性別</label>
+                <select
+                  className="form-control"
+                  id="registerGender"
+                  onChange={()=>{}}
+                >
+                  <option value="1">
+                    男
+                  </option>
+                  <option value="2">
+                    女
+                  </option>
+                  <option value="3" >
+                    其他
+                  </option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="registerBirth">生日</label>
+                <br />
+                <input
+                  className="form-control"
+                  id="registerBirth"
+                  type="date"
+                  onChange={()=>{}}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="registerLocation">所在地</label>
+                <select
+                  className="form-control"
+                  id="registerLocation"
+                  onChange={()=>{}}
+                >
+                  <option value="TW">
+                    TW 台灣
+                  </option>
+                  <option value="HK">
+                    HK 香港
+                  </option>
+                  <option value="US">
+                    US 美國
+                  </option>
+                  <option value="JP">
+                    JP 日本
+                  </option>
+                </select>
+              </div>
         </form>
       </Modal.Body>
       <Modal.Footer>
-        <p className="register_info" onClick={()=>{setLoginOrRegister(true)}}>已經有帳號嗎？點我登入</p>
+        <p
+          className="register_info"
+          onClick={() => {
+            setLoginOrRegister(true)
+          }}
+        >
+          已經有帳號嗎？點我登入
+        </p>
         <Button onClick={RegisterSubmit}>註冊</Button>
       </Modal.Footer>
     </>
@@ -177,7 +245,7 @@ function MemberLoginModal(props) {
       show={loginModalShow}
       onHide={() => setLoginModalShow(false)}
     >
-    {loginOrRegister ? loginOrRegisterTrue : loginOrRegisterFalse}
+      {loginOrRegister ? loginOrRegisterTrue : loginOrRegisterFalse}
     </Modal>
   )
 

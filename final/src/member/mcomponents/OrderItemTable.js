a5875a700e4c14a02d1dca5b500c7e3332d49cbb
ruby <rubypoohtwm@gmail.com>
2020-11-18 12:21:41 +0800

    修改商品

--- final/src/member/mcomponents/OrderItemTable.js
+++ final/src/member/mcomponents/OrderItemTable.js
@@ -20,7 +20,7 @@ function OrderItemTable(props) {
         <td>{Order_Detail_amount}</td>
         <td>{Order_Detail_price}</td>
         <td>
-          <button className="order-btn" href="">查看商品詳細頁</button>
+          <button className="order-btn" href="http://localhost:3001/productlist/sid25">查看商品詳細頁</button>
         </td>
       </tr>
     </>
